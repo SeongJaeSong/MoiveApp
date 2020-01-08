5eddcf5ac5008f94afc3e0407a918a959dc68fbb
SeongJaeSong <tjdkskgnal61@gmail.com>
2020-01-07 19:06:43 -0800

    Updates

---
+++ precache-manifest.1ee643d98c4829f43a5ff08480eb16dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ebb3f6b844b1321b0faa7dea722d84d",
+    "url": "/MovieApp/index.html"
+  },
+  {
+    "revision": "b390c815537c385a34ed",
+    "url": "/MovieApp/static/css/main.a19cb2dd.chunk.css"
+  },
+  {
+    "revision": "02c803fac3999cf1665c",
+    "url": "/MovieApp/static/js/2.2b9d6ecc.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/MovieApp/static/js/2.2b9d6ecc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b390c815537c385a34ed",
+    "url": "/MovieApp/static/js/main.5c3b14ed.chunk.js"
+  },
+  {
+    "revision": "061907e0dfc9e2b26945",
+    "url": "/MovieApp/static/js/runtime-main.2df38bbd.js"
+  }
+]);
